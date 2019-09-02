30bd426dc5c04a618b9ebf459171084752b8aa42
Victor Velasco <victor.velasco@student.swic.edu>
2019-09-02 18:33:26 -0500

    Fixed typos

--- [Programs]/victorProgram2.py
+++ [Programs]/victorProgram2.py
@@ -4,7 +4,7 @@ import math
 pie = math.pi
 
 # Get input
-print('This program takes the radius of two different circles, calculates the area of both circles, and then compares the area\n')
+print('This program takes the radius of two different circles, calculates the area of both circles, and then compares the area.\n')
 radiusA = float(input('Please enter the radius of the first circle: '))
 radiusB = float(input('Please enter the radius of the second circle: '))
 
@@ -13,13 +13,12 @@ areaA = radiusA * radiusA * pie
 areaB = radiusB * radiusB * pie
 
 # Display
-print('The area of circle 1 is:', format(areaA, ',.2f'), ' and the area of circle 2 is:', format(areaB, ',.2f'))
+print('\nThe area of circle 1 is:', format(areaA, ',.2f'), 'and the area of circle 2 is:', format(areaB, ',.2f'))
 
 # Compare
 if areaA > areaB:
-  print('\n\nCircle 1 is bigger')
+  print('\nCircle 1 is bigger')
 elif areaB > areaA:
-  print('\n\nCircle 2 is bigger')
+  print('\nCircle 2 is bigger')
 else:
-  print('\n\nThe area is the same.')
-
+  print('\nThe area is the same.')
