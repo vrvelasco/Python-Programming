5da76d1afa7c3a8fe44cda3401fc679da3789e05
Victor Velasco <victor.velasco@student.swic.edu>
2019-09-09 20:16:14 -0500

    Sep 9 - Update 2

---
+++ [Week 4] September 9/DayOf Week.py
@@ -0,0 +1,20 @@
+# Victor Velasco (DayOfWeek) 9/9/19
+
+day = int(input('Enter a number between 1 and 7: '))
+
+if day == 1:
+    print('Monday')
+elif day == 2:
+    print('Tuesday')
+elif day == 3:
+    print('Wednesday')
+elif day == 4:
+    print('Thursday')
+elif day == 5:
+    print('Friday')
+elif day == 6:
+    print('Saturday')
+elif day == 7:
+    print('Sunday')
+else:
+    print('Error: Please enter a number between 1 and 7.')
