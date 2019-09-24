3b9f189e0efff9ad0bee1723f0f13257b2ae37d8
Victor Velasco <victor.velasco@student.swic.edu>
2019-09-24 18:15:25 -0500

    Sep 24 Update

--- [Exams]/victorExam1 -Corrected-.py
+++ [Exams]/victorExam1 -Corrected-.py
@@ -10,12 +10,18 @@ counter = 0
 
 # Get days
 days = int(input('Please enter the number of day(s): '))
+#To prevent a divide by zero error we need to prevent the user from entering 0.
+while days <= 0:
+    days = int(input('Incorrect entry.\nPlease try again.\n'))
 
 print() # Formatting - Adds blank line
 
 # Get sales
 for i in range(counter, days, 1):
     sales = float(input(f'Please enter day {i + 1} sales amount: ')) # Formatting - Displays the day number
+    #We can also prevent zero for sales.
+    while sales <= 0:
+        sales = float(input('Incorrect entry.\nPlease try again.\n'))
     total += sales
 
 # Math
