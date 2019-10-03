65e8be5ca7b758142d18be61e3fee552db6fefce
Victor Velasco <victor.velasco@student.swic.edu>
2019-10-02 20:35:07 -0500

    Completed functions

--- [Programs]/victorProgram3.py
+++ [Programs]/victorProgram3.py
@@ -2,10 +2,34 @@
 
 # Functions
 def main():
+    t1 = float(input('Please enter the FIRST test score: '))
+    t2 = float(input('\nPlease enter the SECOND test score: '))
+    t3 = float(input('\nPlease enter the THIRD test score: '))
+    t4 = float(input('\nPlease enter the FOURTH test score: '))
+    t5 = float(input('\nPlease enter the FIFTH test score: '))
+
+    # Calculate Average
+    average = calc_avg(t1, t2, t3, t4, t5)
     
+    # Get Letter Grades
+    g1 = get_grade(t1)
+    g2 = get_grade(t2)
+    g3 = get_grade(t3)
+    g4 = get_grade(t4)
+    g5 = get_grade(t5)
+    
+    # Display Results
+    print('Score\tNumeric Score\tLetter Grade')
+    print('Score 1:\t' + format(t1, ',.1f') + '\t' + g1)
+    print('Score 2:\t' + format(t2, ',.1f') + '\t' + g2)
+    print('Score 3:\t' + format(t3, ',.1f') + '\t' + g3)
+    print('Score 4:\t' + format(t4, ',.1f') + '\t' + g4)
+    print('Score 5:\t' + format(t5, ',.1f') + '\t' + g5)
+    print('Average Score: \t')
+
 def calc_avg(t1, t2, t3, t4, t5):
-    average = (t1 * t2 * t3 * t4 * t5) / 5
-    return average
+    calculated = (t1 * t2 * t3 * t4 * t5) / 5
+    return calculated
 
 def get_grade(t):
     if average >= 90:
@@ -16,8 +40,8 @@ def get_grade(t):
         letterGrade = 'C'
     elif average >= 60 and average <= 69:
         letterGrade = 'D'
-    else:
-        letterGrade = 'F'\
+    else: 
+        letterGrade = 'F'
     return letterGrade
     
 # Call main
